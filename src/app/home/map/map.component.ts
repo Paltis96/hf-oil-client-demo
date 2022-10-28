d38073c86d15875f247053388fe31e8b89fd5d15
Denis Kovshun <deniskovshun@gmail.com>
2022-10-28 03:56:02 +0300

    Code refactoring, add state layer

--- src/app/home/map/map.component.ts
+++ src/app/home/map/map.component.ts
@@ -1,17 +1,15 @@
 import { Component, OnChanges, OnDestroy, Input } from '@angular/core';
-import {
-  map,
-  Map,
-  layerGroup,
-  tileLayer,
-  TileLayer,
-  geoJSON,
-  circleMarker,
-  // Marker
-  // icon,
-} from 'leaflet';
+import { map, Map, tileLayer, TileLayer, geoJSON, circleMarker } from 'leaflet';
+import { HttpClient } from '@angular/common/http';
 import { FeatureCollection, GeoJSON } from 'geojson';
-import { point, distance, bearingToAzimuth, bearing } from '@turf/turf';
+import {
+  point,
+  featureCollection,
+  distance,
+  bearingToAzimuth,
+  bearing,
+  featureEach,
+} from '@turf/turf';
 
 @Component({
   selector: 'app-map',
@@ -19,21 +17,19 @@ import { point, distance, bearingToAzimuth, bearing } from '@turf/turf';
   styleUrls: ['./map.component.scss'],
 })
 export class MapComponent implements OnChanges, OnDestroy {
+  constructor(private http: HttpClient) {}
+
   @Input() data: any;
   @Input() loaded: any;
 
+  public uid = Date.now().toString();
+
   private map: Map | any;
-  private borders: GeoJSON | any;
-  private cytiesMarkers = layerGroup();
+  private borders: FeatureCollection | any;
+  private tooltips: FeatureCollection | any;
 
-  // private myIcon = icon({
-  //   iconUrl: 'assets/icon/pin.png',
-  //   iconSize: [32, 32],
-  //   tooltipAnchor: [0, -8],
-  // });
-
-  private initMap(): void {
-    this.map = map('map', { center: [39.8282, -98.5795], zoom: 3 });
+  initMap(): void {
+    this.map = map(this.uid, { center: [39.8282, -98.5795], zoom: 3 });
     const tiles: TileLayer = tileLayer(
       'https://services.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}',
       {
@@ -43,71 +39,77 @@ export class MapComponent implements OnChanges, OnDestroy {
           '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
       }
     ).addTo(this.map);
+    this.getStates(this.map);
+    this.tooltips = geoJSON(this.pointsToGeoJson(this.data), {
+      onEachFeature: (feature, layer) => {
+        this.map.on('zoomend', () => {
+          const props = feature.properties;
+          const city = props.city;
+          const price = props.parsedPrice;
+          layer
+            .bindTooltip(
+              `<div class="leaflet-tooltip-wrapper"> 
+                <div class="leaflet-tooltip-title">${city}</div>
+                <div class="leaflet-tooltip-content"> 
+                  <small>$</small> 
+                  <span>${price[0]}.${price[1]}</span>
+                  <small>${price[2]}</small>
+                </div>
+              </div>`,
+              { direction: this.getDir(props.distMatrix[0]), permanent: true }
+            )
+            .openTooltip();
+        });
+      },
+      pointToLayer: function (_, latlng) {
+        return circleMarker(latlng, {
+          radius: 0,
+          opacity: 0,
+        });
+      },
+    }).addTo(this.map);
+    this.map.fitBounds(this.tooltips.getBounds(), { padding: [50, 50] });
   }
-  private calcDirection(data: any[]) {
+  getDir = (data) => {
+    const [idx, dist, azimuth] = data;
+    let direction = '';
     const metresPerPixel =
       (40075016.686 *
         Math.abs(Math.cos((this.map.getCenter().lat * Math.PI) / 180))) /
       Math.pow(2, this.map.getZoom() + 8);
 
-    const processedData: any = [];
-    let direction = '';
-    data.forEach((first, index) => {
-      const firstPoint = point([first.lng, first.lat]);
-      processedData[index] = [];
-      data.forEach((second) => {
-        if (first.zip !== second.zip) {
-          const secondPoint = point([second.lng, second.lat]);
-          const dist = distance(firstPoint, secondPoint, { units: 'meters' });
-
-          if (dist < metresPerPixel * 100) {
-            const azimuth = bearingToAzimuth(bearing(firstPoint, secondPoint));
-            if (azimuth >= 315 && azimuth <= 45) {
-              direction = 'bottom';
-            } else if (azimuth >= 45 && azimuth <= 135) {
-              direction = 'left';
-            } else if (azimuth >= 135 && azimuth <= 225) {
-              direction = 'top';
-            } else if (azimuth >= 225 && azimuth <= 315) {
-              direction = 'right';
-            }
-            processedData[index].push({ ...first, direction, azimuth, dist });
-          } else {
-            direction = 'center';
-            processedData[index].push({ ...first, direction, dist });
-          }
-        }
-      });
-    });
-
-    return processedData.map((element) => {
-      return element.sort(function (a, b) {
-        return a.dist - b.dist;
-      })[0];
+    if (dist < metresPerPixel * 100) {
+      if (azimuth >= 315 && azimuth <= 45) {
+        direction = 'bottom';
+      } else if (azimuth >= 45 && azimuth <= 135) {
+        direction = 'left';
+      } else if (azimuth >= 135 && azimuth <= 225) {
+        direction = 'top';
+      } else if (azimuth >= 225 && azimuth <= 315) {
+        direction = 'right';
+      }
+      return direction;
+    } else {
+      direction = 'center';
+      return direction;
+    }
+  };
+  getStates(map: Map) {
+    this.http.get('assets/us_state.json').subscribe((json: any) => {
+      this.borders = geoJSON(json, {
+        style: { weight: 1, opacity: 0.5, color: '#0E8BDE', fill: false },
+      }).addTo(map);
     });
   }
-  private fitToBoubds = (data: any) => {
-    const circleBounds = [];
-    // Create city markers
-    data.forEach((element) => {
-      const lng = element.lng;
-      const lat = element.lat;
-      circleBounds.push([lat, lng]);
-    });
-    this.map.fitBounds(circleBounds);
-  };
-  private async makePoint(data: any[], map: Map) {
-    if (this.cytiesMarkers.getLayers().length > 0)
-      this.cytiesMarkers.clearLayers();
+  pointsToGeoJson(data: any[]) {
+    const pointList = [];
 
-    const points = this.calcDirection(data);
-    console.log(points);
-    points.forEach((element) => {
-      const lng = element.lng;
-      const lat = element.lat;
-      const price = element.price.toString();
+    data.forEach((item) => {
+      console.log(item);
       let parsedPrice =
-        price.indexOf('.') !== -1 ? price.split('.') : [price, '000'];
+        item.price.toString().indexOf('.') !== -1
+          ? item.price.toString().split('.')
+          : [item.price, '000'];
       if (parsedPrice[1].length <= 3)
         while (parsedPrice[1].length <= 3) {
           parsedPrice[1] = parsedPrice[1] + '0';
@@ -117,62 +119,35 @@ export class MapComponent implements OnChanges, OnDestroy {
         parsedPrice[1].substring(0, 2),
         parsedPrice[1].substring(2, 3),
       ];
-      const circle = circleMarker([lat, lng], {
-        opacity: 0,
-        fillOpacity: 0,
-        radius: 0,
-      })
-        .bindTooltip(
-          `<div class="leaflet-tooltip-wrapper"> 
-                <div class="leaflet-tooltip-title">${element.city}</div>
-                <div class="leaflet-tooltip-content"> 
-                  <small>$</small> 
-                  <span>${parsedPrice[0]}.${parsedPrice[1]}</span>
-                  <small>${parsedPrice[2]}</small>
-                </div>
-              </div>`,
-          { direction: element.direction, permanent: true }
-        )
-        .openTooltip();
-      this.cytiesMarkers.addLayer(circle);
+      const p = point([item.lng, item.lat], { ...item, parsedPrice });
+      pointList.push(p);
     });
-    this.cytiesMarkers.addTo(map);
-  }
-  private makePolygons(data: any[], map: Map): void {
-    if (this.borders) this.borders.remove();
 
-    const collection: FeatureCollection = {
-      type: 'FeatureCollection',
-      features: [],
-    };
-    // add geojson features data in collection
-    data.forEach((element) => {
-      const fiture = JSON.parse(element.geom);
-      fiture.properties = element.min_price;
-      collection.features.push(fiture);
-    });
-    this.borders = geoJSON(collection, {
-      onEachFeature: (feature, layer) => {
-        layer
-          .bindTooltip(`<b>$ ${feature.properties}</b>`, {
-            direction: 'top',
-            permanent: true,
-          })
-          .openTooltip();
-      },
-    }).addTo(map);
-    map.fitBounds(this.borders.getBounds());
-  }
+    const collection = featureCollection(pointList);
 
-  async ngOnChanges() {
-    if (this.loaded && !this.map) this.initMap();
-    // if (this.map && this.data) this.makePolygons(this.data, this.map);
-    if (this.map && this.data) {
-      this.fitToBoubds(this.data);
-      this.map.on('zoomend', () => {
-        this.makePoint(this.data, this.map);
+    featureEach(collection, function (firstFeature, firstIndex) {
+      const props = firstFeature.properties;
+      const distMatrix = (props.distMatrix = []);
+      featureEach(collection, function (secondFeature, secondIndex) {
+        if (firstIndex != secondIndex) {
+          const dist = distance(firstFeature as any, secondFeature as any, {
+            units: 'meters',
+          });
+          const azimuth = bearingToAzimuth(
+            bearing(firstFeature as any, secondFeature as any)
+          );
+          distMatrix.push([secondIndex, dist, azimuth]);
+        }
       });
-    }
+
+      distMatrix.sort(function (a, b) {
+        return a[1] - b[1];
+      });
+    });
+    return collection;
+  }
+  ngOnChanges() {
+    if (this.loaded && !this.map) this.initMap();
   }
   ngOnDestroy(): void {
     this.map.remove();
